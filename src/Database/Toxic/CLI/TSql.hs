250335fc8493bd29d9739ec0849931161934a4fa
Michael Burge <michaelburge@pobox.com>
2015-05-01 18:02:32 -0700

    Work on integrating networking into the tsql CLI

--- src/Database/Toxic/CLI/TSql.hs
+++ src/Database/Toxic/CLI/TSql.hs
@@ -1,18 +1,36 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Database.Toxic.CLI.TSql where
 
+import Control.Concurrent
+import Control.Lens
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
-import Control.Monad.Trans.State.Strict
+import Control.Monad.Trans.State
 import qualified Data.Text as T
 import System.Console.Haskeline
 
 import Database.Toxic.TSql.AST
+import Database.Toxic.TSql.Handler
 import Database.Toxic.TSql.Parser
+import Database.Toxic.TSql.Protocol
 
-data InterpreterState = InterpreterState { } deriving (Eq, Show)
+data InterpreterState = InterpreterState {
+  _interpreterHandlerState :: HandlerState,
+  _interpreterHandlerThread :: ThreadId
+  }
 
-mkInterpreterState :: InterpreterState
-mkInterpreterState = error "mkInterpreterState: Implement me!"
+makeLenses ''InterpreterState
+
+mkInterpreterState :: IO InterpreterState
+mkInterpreterState = do
+  (handlerThread, handlerState) <- handlerConnect
+  let chanAction = handlerState ^. handlerAction
+  writeChan chanAction $ ActionSendStartupMessage defaultStartupMessage
+  return InterpreterState {
+    _interpreterHandlerState = handlerState,
+    _interpreterHandlerThread = handlerThread
+    }
 
 interpreterSettings :: MonadIO m => Settings m
 interpreterSettings = defaultSettings {
@@ -20,17 +38,37 @@ interpreterSettings = defaultSettings {
   historyFile = Just ".tsql_history"
   }
 
+handleNotification :: HandlerNotification -> StateT InterpreterState (InputT IO) ()
+handleNotification notification = case notification of
+  NotificationNetworkSend bs -> lift $ outputStrLn $ "Sent bytes: " ++ show bs
+  NotificationNetworkReceive bs -> lift $ outputStrLn $ "Received bytes: " ++ show bs
+
+handleNotifications :: StateT InterpreterState (InputT IO) ()
+handleNotifications = do
+  state <- get
+  let chan = state ^. interpreterHandlerState ^. handlerNotify
+  notifications <- liftIO $ getChanContents chan
+  mapM_ handleNotification notifications
+
+runHandler :: StateT InterpreterState (InputT IO) ()
+runHandler = do
+  state <- get
+  liftIO $ evalStateT handlerLoop $ state ^. interpreterHandlerState
+
 runCommand :: Command -> StateT InterpreterState (InputT IO) ()
 runCommand command = case command of
   CStatement statement -> lift $ outputStrLn $ "Received command: " ++ show command
 
 tsqlMain :: IO ()
-tsqlMain =
+tsqlMain = do
+  defaultInterpreterState <- mkInterpreterState
   runInputT interpreterSettings $
-  evalStateT loop mkInterpreterState
+    evalStateT loop defaultInterpreterState
   where
     loop :: StateT InterpreterState (InputT IO) ()
     loop = do
+      handleNotifications
+      runHandler
       minput <- lift $ getInputLine "> "
       case minput of
         Nothing -> return ()
@@ -40,4 +78,5 @@ tsqlMain =
             Left parse_error -> lift $ outputStrLn $
                                 "Parse error: " ++ show parse_error
             Right command -> runCommand command
+          
           loop
--- src/Database/Toxic/TSql/Handler.hs
+++ src/Database/Toxic/TSql/Handler.hs
@@ -2,32 +2,69 @@
 
 module Database.Toxic.TSql.Handler where
 
+import Control.Concurrent
 import Control.Lens
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
+import qualified Data.Binary as B
+import qualified Data.Binary.Put as B
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Data.Vector as V
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 
 import Database.Toxic.TSql.Protocol
 
-data HandleState = HandleState {
-  _handleSocket :: Socket
-  } deriving (Eq, Show)
+maximumMessageSize = 1000;
 
-makeLenses ''HandleState
+data HandlerState = HandlerState {
+  _handlerSocket   :: Socket,
+  _handlerNotify   :: Chan HandlerNotification,
+  _handlerAction   :: Chan HandlerAction
+  }
 
-connect :: StateT HandleState IO ()
-connect = do
+data HandlerNotification =
+    NotificationNetworkSend BS.ByteString
+  | NotificationNetworkReceive BS.ByteString
+  deriving (Eq, Show)
+
+data HandlerAction =
+    ActionSendStartupMessage StartupMessage
+  deriving (Eq, Show)
+
+makeLenses ''HandlerState
+
+handlerConnect :: IO (ThreadId, HandlerState)
+handlerConnect = do
   let family = AF_UNIX
       socketType = Stream
       protocolNumber = defaultProtocol
-  mySocket <- lift $ socket family socketType protocolNumber
-  modify (& handleSocket .~ mySocket)
+      address = SockAddrUnix "/var/run/postgresql/.s.PGSQL.5432"
+  mySocket <- socket family socketType protocolNumber
+  connect mySocket address
+  notifyChannel <- newChan
+  actionChannel <- newChan
+  let initialState = HandlerState {
+        _handlerSocket = mySocket,
+        _handlerNotify = notifyChannel,
+        _handlerAction = actionChannel
+        }
+  return (error "handlerConnect: threadId shouldn't be used", initialState)
 
-startupMessage :: StartupMessage
-startupMessage = StartupMessage {
+handlerLoop :: StateT HandlerState IO ()
+handlerLoop = do
+  state <- get
+  actions <- lift $ getChanContents $ state ^. handlerAction
+  mapM_ handleAction actions
+
+handleAction :: HandlerAction -> StateT HandlerState IO ()
+handleAction action = case action of
+  ActionSendStartupMessage startupMessage -> processSendStartupMessage startupMessage
+
+defaultStartupMessage :: StartupMessage
+defaultStartupMessage = StartupMessage {
+  startupMessageProtocolVersion = defaultProtocolVersion,
   startupMessageParameters = V.fromList [
      ("user", "mburge"),
      ("database", "mburge"),
@@ -35,9 +72,31 @@ startupMessage = StartupMessage {
      ("client_encoding", "UTF8")
      ]
   }
-                                
--- sendStartupMessage :: StateT HandleState IO ()
--- sendStartupMessage = do
---   state <- get
---   let message = startupMessage
---       serializedMessage = runPut 
+
+handlerSend :: BS.ByteString -> StateT HandlerState IO ()
+handlerSend message = do
+  state <- get
+  let notifyChannel = state ^. handlerNotify
+      socket = state ^. handlerSocket
+  lift $ do
+    writeChan notifyChannel $ NotificationNetworkSend message
+    send socket message
+    return ()
+
+handlerReceive :: StateT HandlerState IO BS.ByteString
+handlerReceive = do
+  state <- get
+  let notifyChannel = state ^. handlerNotify
+      socket = state ^. handlerSocket
+  lift $ do
+    message <- recv socket maximumMessageSize
+    writeChan notifyChannel $ NotificationNetworkReceive message
+    return message
+
+processSendStartupMessage :: StartupMessage -> StateT HandlerState IO ()
+processSendStartupMessage startupMessage = do
+  state <- get
+  let message = startupMessage
+      serializedMessage = BSL.toStrict $ B.runPut $ B.put message
+  handlerSend serializedMessage
+  handlerReceive >> return ()
