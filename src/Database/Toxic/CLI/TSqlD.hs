f6f1e2f201fdb5fd05ac8cfafbef7dbedb89d83a
Michael Burge <michaelburge@pobox.com>
2015-05-02 19:21:16 -0700

    Incrementally parse incoming messages

--- src/Database/Toxic/CLI/TSqlD.hs
+++ src/Database/Toxic/CLI/TSqlD.hs
@@ -1,19 +1,27 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Database.Toxic.CLI.TSqlD where
 
 import Database.Toxic.TSql.Protocol
 
 import Control.Concurrent
 import Control.Exception
+import Control.Lens
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
-import Data.Binary
+import qualified Data.Binary as B
+import Data.Binary.Get
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import System.Directory
 
-data SessionState = SessionState { } deriving (Eq, Show)
+data SessionState = SessionState {
+  _sessionStateMessage :: Decoder AnyMessage
+  }
+
+makeLenses ''SessionState
 
 maximumReceiveLength = 10000
 
@@ -41,18 +49,39 @@ handleMessage message = case message of
   MStartupMessage x -> handleStartupMessage x
   MQuery x -> handleQuery x
 
+handleNewInput :: BS.ByteString -> StateT SessionState IO ()
+handleNewInput bs = do
+  state <- get
+  let decoder = state ^. sessionStateMessage
+      newDecoder = pushEndOfInput $
+                   pushChunk decoder bs
+  modify $ (& sessionStateMessage .~ newDecoder)
+  case newDecoder of
+    Fail unconsumed offset errorMessage ->
+      error $ "Failed to decode:" ++ show (unconsumed, offset, errorMessage)
+    Partial f -> do
+      lift $ putStrLn $ "Partial decoder"
+      return ()
+    Done unconsumed offset message -> do
+      lift $ putStrLn $ "Interpreted message: " ++ show message
+      handleMessage message
+      modify $ (& sessionStateMessage .~ runGetIncremental B.get)
+      if BS.null unconsumed
+        then return ()
+        else handleNewInput unconsumed
+
 serverHandler :: (Socket, SockAddr) -> IO ()
 serverHandler (clientSocket, clientAddress) = do
-  let initialState = SessionState
+  let initialState = SessionState {
+        _sessionStateMessage = runGetIncremental B.get
+        }
   evalStateT (loop clientSocket clientAddress) initialState
   where
     loop :: Socket -> SockAddr -> StateT SessionState IO ()
     loop clientSocket clientAddress = do
       networkMessage <- lift $ recv clientSocket maximumReceiveLength
       lift $ putStrLn $ "Received bytes: " ++ show networkMessage
-      let message = decode $ BSL.fromStrict networkMessage :: AnyMessage
-      lift $ putStrLn $ "Interpreted message: " ++ show message
-      handleMessage message
+      handleNewInput networkMessage
       loop clientSocket clientAddress
 
 cleanup :: Socket -> IO ()
